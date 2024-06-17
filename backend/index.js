601fdd9dcd334bece21b9a56265bf7b1dbbd1555
francis-mwas <mwangif184@gmail.com>
2024-06-17 18:57:18 +0300

    passport configurations

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,44 @@ import { ApolloServer } from '@apollo/server';
 import dotenv from 'dotenv';
 import { expressMiddleware } from '@apollo/server/express4';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { buildContext } from 'graphql-passport';
+
+import passport from 'passport';
+import session from 'express-session';
+import connectMongo from 'connect-mongodb-session';
 
 import mergedResolvers from './resolvers/index.js';
 import mergedTypeDefs from './typeDefs/index.js';
 import { connectDB } from './dbConfigs/dbConnect.js';
+import { configurePassport } from './passport/passport.config.js';
 
 dotenv.config();
+configurePassport();
 const app = express();
 const httpServer = http.createServer(app);
+
+const MongoDBStore = connectMongo(session);
+const store = new MongoDBStore({
+  uri: process.env.mongo_URI,
+  collection: 'mySessions',
+});
+
+store.on('error', (err) => console.log(err));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true,
+    },
+    store,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
@@ -24,16 +54,16 @@ await server.start();
 
 app.use(
   '/',
-  cors(),
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }),
   express.json(),
-  // expressMiddleware accepts the same arguments:
-  // an Apollo Server instance and optional configuration options
   expressMiddleware(server, {
-    context: async ({ req }) => ({ req }),
+    context: async ({ req, res }) => buildContext({ req, res }),
   })
 );
 
-// Modified server startup
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
 await connectDB();
 
