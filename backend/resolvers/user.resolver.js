6afbd7625e4ac9f2792e4a3e0a50d5336cdf9795
francis-mwas <mwangif184@gmail.com>
2024-06-17 19:07:36 +0300

    added signup resolver

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -1,4 +1,8 @@
 import { users } from '../dummydata/data.js';
+import Transaction from '../models/transaction.model.js';
+import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+
 
 const userResolver = {
   Query: {
@@ -9,6 +13,41 @@ const userResolver = {
       return users.find((user) => user._id === userId);
     },
   },
-  Mutation: {},
+  Mutation: {
+    signUp: async (_, { input }, context) => {
+      try {
+        const { username, name, password, gender } = input;
+
+        if (!username || !name || !password || !gender) {
+          throw new Error('Please fill all the fields');
+        }
+        const userExists = await User.findOne({ username });
+        if (userExists) {
+          throw new Error('User already registered!');
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // https://avatar-placeholder.iran.liara.run/
+        let boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+        const newUser = new User({
+          username,
+          name,
+          password: hashedPassword,
+          gender,
+          profilePicture: gender === 'male' ? boyProfilePic : girlProfilePic,
+        });
+
+        await newUser.save();
+        await context.login(newUser);
+        return newUser;
+      } catch (err) {
+        console.error('Error while signing up: ', err);
+        throw new Error(err.message || 'Internal server error');
+      }
+    },
+  },
 };
 export default userResolver;
